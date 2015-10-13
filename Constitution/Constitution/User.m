3eede3b7dfa7bbc67b519c66b5d62fe1ebeb5b21
Fernando Lizana <flizanab@gmail.com>
2015-10-13 19:02:08 -0300

    User methods declared in implementation file

--- Constitution/Constitution/User.m
+++ Constitution/Constitution/User.m
@@ -26,4 +26,106 @@
 
 @implementation User
 
+#pragma mark - Instance Methods
+
+- (NSInteger)getUserId
+{
+    // TODO: return user id
+    return 0;
+}
+
+- (void)setPassword:(NSString *)password
+{
+    // TODO: set password
+}
+
+- (NSString *)getFirstName
+{
+    // TODO: return first name
+    return @"";
+}
+
+- (void)setFirstName:(NSString *)firstName
+{
+    // TODO: set first name
+}
+
+- (NSString *)getLastName
+{
+    // TODO: return last name
+    return @"";
+}
+
+- (void)setLastName:(NSString *)lastName
+{
+    // TODO: set last name
+}
+
+- (NSString *)getEmail
+{
+    // TODO: return email
+    return @"";
+}
+
+- (void)setEmail:(NSString *)email
+{
+    // TODO: set email
+}
+
+- (void)setGender:(NSString *)gender
+{
+    // TODO: set gender
+}
+
+- (void)setBirthDate:(NSDate *)birthDate
+{
+    // TODO: set birth date
+}
+
+- (void)setAge:(NSInteger)age
+{
+    // TODO: set age
+}
+
+- (void)setRegion:(NSString *)region
+{
+    // TODO: set region
+}
+
+- (void)setCity:(NSString *)city
+{
+    // TODO: set city
+}
+
+- (NSString *)getUserToken
+{
+    // TODO: get user token
+    // This is more tricky, first retrieve from keychain and then return it
+    return @"";
+}
+
+- (void)setUserToken:(NSString *)userToken
+{
+    // TODO: set user token
+    // This is more tricky, save token into keychain
+}
+
+- (void)signUp:(void (^)(BOOL, NSError *))result
+{
+    // TODO: sign up user against server
+}
+
+- (void)save:(void (^)(BOOL, NSError *))result
+{
+    // TODO: save user against server
+}
+
+
+#pragma mark - Class Methods
+
++ (void)logInWithEmail:(NSString *)email password:(NSString *)password block:(void (^)(BOOL, NSError *))result
+{
+    // TODO: log in with credentials against server
+}
+
 @end
