a3920e45936cccd4efc46f7495191ce0a0a9dfa2
Fernando Lizana <flizanab@gmail.com>
2015-10-15 12:59:18 -0300

    Added keychain header file

--- Constitution/Constitution/KeychainAccess.h
+++ Constitution/Constitution/KeychainAccess.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Security/Security.h>
 
 @interface KeychainAccess : NSObject
+{
+    NSMutableDictionary *keychainData;
+    NSMutableDictionary *genericPasswordQuery;
+}
+
+@property (strong, nonatomic) NSMutableDictionary *keychainData;
+@property (strong, nonatomic) NSMutableDictionary *genericPasswordQuery;
+
+- (BOOL)mySetObject:(id)inObject forKey:(id)key;
+- (id)myObjectForKey:(id)key;
+- (BOOL)deleteObjectForKey:(id)key;
 
 @end
